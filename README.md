7a2621001bcb642ca0dd20bc3b45db1106ef5025
crispy whiskers <nguyen.k.allen@gmail.com>
2023-12-16 22:22:31 -0500

    implement aqua webui copy text export

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # ONGEKI-score-analyzer
 Find the worth of multiple ONGEKI scores.
+
+# Use
+
+This script requires any `Python 3` installation.
+
+### Aqua Webui
+This is primarily written with the Aqua webui in mind. Do not use the Aqua JSON export function, it does not contain enough information for a rating breakdown. First, open the Rating page in the webui. Hit `CTRL+A` and copy, then paste the contents into a text file. The text file will look like this: 
+
+```
+Aqua Viewer
+Rating
+Current Rating: 13.63
+Highest Rating: 13.63
+Total Best 30
+Music	Level	Score 1: Re：End of a Dream 	Expert Lv. 12.7	1000473
+2: CO5M1C R4ILR0AD 	Master Lv. 13.8	982064
+3: 光線チューニング 	Master Lv. 12.8	996718
+4: STARLIGHT TWILIGHT -GC edit- 	Master Lv. 13.8	975839
+5: Halcyon 	Master Lv. 13.4	983198
+...
+```
+
+Don't worry about the first score being in line with the table header. This is accounted for.
+
+Run the script like this: 
+```
+python3 analyzer.py your_score_file.txt
+```
+
+### CSV input
+Not really sure how other servers like Artemis export. For everything else, there is `csv`. Make sure the csv has the following format:
--- analyzer.py
+++ analyzer.py
@@ -1,4 +1,4 @@
-import math, re
+import math, re, sys
 
 table = []
 
@@ -9,24 +9,23 @@ class Play:
         self.level = level
         self.difficulty = difficulty
 
-        # stolen from ChunithmRatingBreakdown, adjusted for ongeki
+        # stolen from OngekiScoreLog https://github.com/project-primera/ongeki-score
+        # also adapted part of chunithmratingbreakdown
         levelBase = level*100
         rating = 0
         if score >= 1_007_500:
             rating = levelBase + 200
         elif score >= 1_000_000:
-            rating = levelBase + 150 + (score - 1_000_000) / 7_500 * 500
-        elif score >= 990_000:
-            rating = levelBase + 100 + (score - 990_000) / 10_000 * 500
+            rating = math.floor((levelBase + 150) + (math.floor((score - 1000000) / 150))) / 100
         elif score >= 970_000:
-            rating = levelBase + (score - 970_000) / 20_000 * 500
-        elif score >= 940_000:
-            rating = levelBase - 500 + (score - 940_000) / 30_000 * 500
-        elif score >= 900_000:
-            rating = (levelBase - 500) / 2 + ((score - 900_000)
-                                              * ((levelBase - 500) / 2)) / 100_000
+            rating = math.floor(levelBase + (math.floor((score - 970000) / 200))) / 100
+            #rating = round((levelBase + (score - 970_000) / 30_000 * 150)/100, 2)
+        else:
+            #according to the gamerch ongeki wiki, below 900k this formula is not verified
+            rating = math.floor(levelBase - (math.floor((970000 - score) / 175 + 1))) / 100
+            rating = 0 if rating < 0 else rating
 
-        self.rating = rating/100
+        self.rating = rating
 
     def __str__(self):
         return f'{math.floor(self.rating*100)/100:.2f}\tfrom '+'{:,}'.format(self.score)+f'\ton {self.level}\t{self.title} {self.difficulty}'
@@ -43,11 +42,61 @@ class Play:
     def __repr__(self):
         return self.__str__()
 
+class Table:
+    def __init__(self):
+        self.scores = [] 
+    
+    def __str__(self):
+        topavg = round(sum([x.rating for x in self.scores]) / len(self.scores) , 7)
+        # average with rounding to prevent floating point errors
+
+        return( f'Table of {str(len(self.scores))}:\n'+'\n'.join([str(x) for x in sorted(self.scores, reverse=True)]) +
+                f'\nAverage: {topavg}' )
+        # overall rating is always rounded down by two decimal places, but keeping raw decimal
+        # so you can see exactly where you are
+
 
 if __name__ == "__main__":
-    #f = open(sys.argv[1], encoding='utf-8').read()
+    f = open(sys.argv[1], encoding='utf-8').read()
 
-    linepattern = r'\d\:\s()'
+    linepattern = r'(\t?Music	Level	Score )?\d+: (.*?)\t(Expert|Master|Advanced) Lv\. (\d\d\.\d)\t(\d+)'
+    #print(re.match(linepattern, 'Music	Level	Score 1: Re：End of a Dream 	Expert Lv. 12.7	1000473').groups())
+    lines = f.splitlines()
+    status = 0
 
-    p = Play(12.7, 1000473, 'Re:End of a Dream', 'EXPERT')
-    print(str(p))
+    bo30 = Table()
+    new15 = Table()
+    recent10 = Table() #10 best of 50
+
+    for l in lines:
+        if l.startswith('Total Best 30'):
+            status = 1
+
+        elif l.startswith('New Song Best 15'):
+            status = 2
+        
+        elif l.startswith('Recent Best 10'):
+            status = 3
+        
+        elif l.startswith('===='):
+            status = 0
+
+        m = re.match(linepattern, l)
+        if not m == None:
+            #print(m.groups())
+            match status:
+                case 1:
+                    bo30.scores.append(Play(float(m.group(4)), int(m.group(5)), m.group(2), m.group(3)))
+                case 2: 
+                    new15.scores.append(Play(float(m.group(4)), int(m.group(5)), m.group(2), m.group(3)))
+                case 3:
+                    recent10.scores.append(Play(float(m.group(4)), int(m.group(5)), m.group(2), m.group(3)))
+                case _:
+                    continue
+
+
+    print(str(bo30))
+    print()
+    print(str(new15))
+    print()
+    print(str(recent10))
